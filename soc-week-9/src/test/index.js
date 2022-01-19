fdd940cfb9ce9df529e0eef4e642dcba83bb7c3f
Kagublanca <reecekagu@gmail.com>
2022-01-19 13:42:00 +0000

    ENV

--- soc-week-9/src/test/index.js
+++ soc-week-9/src/test/index.js
@@ -3,3 +3,4 @@ const arrayData = [1,1,1,1,2,2,1,2,4,2,5,2,6,2,3,1,5,2,4,3,5,3,2,];
 const countOccurrences = (array, val) => array.reduce((a, v) => (v === val ?  a + 1 : a), 0)
 
 console.log(countOccurrences(arrayData, 6));
+
