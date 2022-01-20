21cd34415bbb8c9a8ea23dd00deb2af435c984b0
asaad227 <a.saad227@yahoo.com>
2022-01-20 15:04:50 +0000

    clear the app section

--- soc-week-9/src/component/App/App.js
+++ soc-week-9/src/component/App/App.js
@@ -5,43 +5,7 @@ import UserDisplay from '../UserDisplay';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
-	const [ feeling, setfeeling ] = useState(0);
-	const [ reflection, setreflection ] = useState('');
-	const [ workshop, setWorkshop ] = useState('');
-	const [ work, setwork ] = useState('');
 
-	const object = {
-		feeling: feeling,
-		reflection: reflection,
-		workshop: workshop,
-		work: work
-	};
-
-	async function getApi() {
-		const response = await fetch('https://soc-app-17.herokuapp.com/users');
-		const data = await response.json();
-		console.log(data);
-	}
-}
-
-async function postApi() {
-	const response = await fetch('https://soc-app-17.herokuapp.com/users', {
-		method: 'POST',
-		mode: 'no-cors',
-		headers: {
-			'Content-type': 'application/json'
-		},
-
-		body: JSON.stringify(object)
-	});
-	const data = await response.json();
-	console.log(data);
-
-	function handleClick(e) {
-		e.preventDefault();
-		getApi();
-		postApi();
-	}
 
 	return (
 		<Routes>
@@ -50,7 +14,7 @@ async function postApi() {
 				element={
 					<div>
 						<NavBar />
-						<SearchForm setfeeling={setfeeling} handleClick={handleClick} />
+						<SearchForm />
 					</div>
 				}
 			/>
